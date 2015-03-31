212b2031b232b0a54dda3bbdb04ce17760195ceb
Masahiko Higashiyama <higashiyama@scaleout.jp>
2015-03-31 16:46:41 +0900

    changed extconf file.

--- Rakefile
+++ Rakefile
@@ -13,5 +13,4 @@ task :build => :compile
 
 Rake::ExtensionTask.new("ruby_da") do |ext|
   ext.lib_dir = "lib/ruby_da"
-  ext.config_options = "--with-da-include=#{File.dirname(__FILE__)}/libda/include"
 end
--- ext/ruby_da/extconf.rb
+++ ext/ruby_da/extconf.rb
@@ -3,6 +3,7 @@ RbConfig::CONFIG["CPP"] = "g++ -E"
 $LDFLAGS += " -lstdc++"
 
 dir_config('da')
+find_header('double_array.hpp', File.dirname(__FILE__) + "/../../libda/include")
 if have_header('double_array.hpp')
    create_makefile("ruby_da/ruby_da")
 end
